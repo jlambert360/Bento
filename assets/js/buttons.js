9ddf3d9f91c5b7ba2ab742c549d6b4b4d24aa2c3
unknown <birdthulu@gmail.com>
2023-12-01 22:56:40 -0500

    Clicking buttons opens websites in sidebar

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -8,7 +8,7 @@ const generateFirstButtonsContainer = () => {
 		let item = `
         <a
           href="${button.link}"
-          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          target="sidebarFrame" // target="${CONFIG.openInNewTab ? '_blank' : ''}"
           class="card button button__${button.id}"
         >
           <i class="buttonIcon" icon-name="${button.icon}"></i>
